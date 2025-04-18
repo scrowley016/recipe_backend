7a9ec217de0dbd802120571aadc95b54a2d3bec6
scrowley016 <33128271+scrowley016@users.noreply.github.com>
2025-04-17 20:04:40 -0400

    please dont break

--- routes/recipe.js
+++ routes/recipe.js
@@ -55,10 +55,11 @@ router.delete("/user-recipes/:id", requireAuth, async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const jsonData = JSON.parse(fs.readFileSync(dataPath, "utf8"));
-    const dbRecipes = await Recipe.findAll();
-    const merged = [...jsonData, ...dbRecipes.map((recipe) => recipe.toJSON())];
-    // res.json(jsonData);
-    res.json(merged);
+    // const dbRecipes = await Recipe.findAll();
+    // const merged = [...jsonData, ...dbRecipes.map((recipe) => recipe.toJSON())];
+    // console.log("Merged Recipes:", merged);
+    res.json(jsonData);
+    // res.json(merged);
   } catch (err) {
     console.error("Error fetching recipes:", err);
     res.status(500).json({ error: "Failed to load recipes" });
